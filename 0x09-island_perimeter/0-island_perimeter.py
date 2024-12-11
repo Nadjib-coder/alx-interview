d40602ad17b2440b9ff3c0c1f540a202347bdd48
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-11 20:25:22 +0000

    complet the project

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""
-Island Perimeter Problem
+"""Island Perimeter Problem
 """
 
 
@@ -13,16 +12,16 @@ def island_perimeter(grid):
         the perimeter of the island
     """
 
-    x = 0
+    p = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if (grid[i][j] == 1):
                 if (i <= 0 or grid[i - 1][j] == 0):
-                    x += 1
+                    p += 1
                 if (i >= len(grid) - 1 or grid[i + 1][j] == 0):
-                    x += 1
+                    p += 1
                 if (j <= 0 or grid[i][j - 1] == 0):
-                    x += 1
+                    p += 1
                 if (j >= len(grid[i]) - 1 or grid[i][j + 1] == 0):
-                    x += 1
-    return x
+                    p += 1
+    return p
