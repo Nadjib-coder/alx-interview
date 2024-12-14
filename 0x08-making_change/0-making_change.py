1fac571dbadabc956a71ef6bfe13b2282a418a62
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-14 19:32:48 +0000

    update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,33 +1,5 @@
 #!/usr/bin/python3
 
-""" Contains makeChange Function """
-
-
-def makeChange(coins, total):
-    """
-    Returns: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
-    """
-    if not coins or coins is None:
-        return -1
-    if total <= 0:
-        return 0
-    
-    # Initialize a list to track the minimum coins needed for each amount
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # Base case: 0 coins needed to make total 0
-
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    
-    # If the total cannot be reached, return -1
-    return dp[total] if dp[total] != float('inf') else -1
-
-
-#!/usr/bin/python3
-
 # """ Contains makeChange Function"""
 
 
