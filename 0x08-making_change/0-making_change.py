96d054733e38ff4d60efbcda4e4a7fff37b5497a
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-14 19:49:29 +0000

    update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,28 +1,25 @@
 #!/usr/bin/python3
-
-""" Contains makeChange Function"""
+""" Making changes """
 
 
 def makeChange(coins, total):
+    """ Generate changes needed to reach total
+
+    Args:
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
     """
-    Returns: fewest number of coins needed to meet total
-        If total is 0 or less, return 0
-        If total cannot be met by any number of coins you have, return -1
-    """
-    
     if total <= 0:
         return 0
-    
+    check = 0
+    temp = 0
     coins.sort(reverse=True)
-    num_change = 0
-    sum_change = 0
-    
     for i in coins:
-        while sum_change < total:
-            sum_change += i
-            num_change += i
-        if sum_change == total:
-            return num_change
-        sum_change -= i
-        num_change -= i
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
     return -1
