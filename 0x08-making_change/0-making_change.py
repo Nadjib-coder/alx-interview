eb8101c27237b0763b97a9dfe5fd99c733bda938
Nadjib-coder <nadjib.programmation1@gmail.com>
2024-12-14 19:47:27 +0000

    update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -13,24 +13,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
     
-    remainder = total
-    coins_needed = 0
+    coins.sort(reverse=True)
+    num_change = 0
+    sum_change = 0
     
-    coin_index = 0
-    
-    sorted_coins_list = sorted(coins, reverse=True)
-    
-    list_len = len(coins)
-    
-    while remainder > 0:
-        
-        if coin_index >= list_len:
-            return -1
-        
-        if remainder- sorted_coins_list[coin_index] >= 0:
-            remainder -= sorted_coins_list[coin_index]
-            
-            coins_needed += 1
-        else:
-            coin_index += 1
-    return coins_needed
+    for i in coins:
+        while sum_change < total:
+            sum_change += i
+            num_change += i
+        if sum_change == total:
+            return num_change
+        sum_change -= i
+        num_change -= i
+    return -1
