0d9e4258aac07cc534da14c525d0b7f45de82c96
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-01-22 18:45:35 +0000

    update 0x05-nqueens project

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,24 +2,24 @@
 """ N queens """
 import sys
 
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+# Argument handling
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
     exit(1)
 
-if not sys.argv[1].isdigit():
+try:
+    n = int(sys.argv[1])
+except ValueError:
     print("N must be a number")
     exit(1)
 
-if int(sys.argv[1]) < 4:
+if n < 4:
     print("N must be at least 4")
     exit(1)
 
-n = int(sys.argv[1])
-
 
 def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
+    """Find possible positions for queens."""
     if i < n:
         for j in range(n):
             if j not in a and i + j not in b and i - j not in c:
@@ -28,17 +28,11 @@ def queens(n, i=0, a=[], b=[], c=[]):
         yield a
 
 
+# Function to print all solutions
 def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+    """Solve the N Queens problem and print solutions."""
+    for solution in queens(n):
+        print([[i, solution[i]] for i in range(n)])
 
 
 solve(n)
